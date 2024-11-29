0b29cf613c0dde92f10af1a3f8250710d07e49ec
SergioTeco <52510153+SergioTeco@users.noreply.github.com>
2024-11-29 10:32:43 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,33 +1,28 @@
-import express from "express";
-//import routes from "./routes/index.js";
-//import __dirname from "./dirname.js";
-const session = require('express-session');
-import { Server } from "socket.io";
-const passport = require('./config/passport'); // Estrategia local
-import { connectMongoDB } from "./config/mondoDB.js";
-//import session from "express-session";
-//const authRoutes = require('./routes/session-local');
-//const jwtAuthRoutes = require('./routes/auth-jwt'); 
-require('./config/passport-local'); 
-require('./config/passport-register'); 
-require('./config/passport-jwt'); 
-const routes = require('./routes'); // Importar las rutas desde index.js
+const express = require("express");
+const session = require("express-session");
+const passport = require("./config/passport"); // Estrategia local
+const { connectMongoDB } = require("./config/mondoDB");
+const routes = require("./routes"); // Importar las rutas desde index.js
+const { Server } = require("socket.io");
+
+require("./config/passport-local");
+require("./config/passport-register");
+require("./config/passport-jwt");
 
 const app = express();
 
-//conexion a mongo
+// Conexion a MongoDB
 connectMongoDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-//config de sesion estrategia local
+// Configuración de sesión con estrategia local
 app.use(
   session({
     secret: "secret",
-    resave: true, // Mantiene la session activa, si esto est el false la session se cierra
-    saveUninitialized: true, // Guarde la sesion
+    resave: true, // Mantiene la sesión activa, si esto es false, la sesión se cierra
+    saveUninitialized: true, // Guarda la sesión
   })
 );
 
@@ -35,18 +30,8 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Rutas de la api
-//app.use('/auth', authRoutes); //se le pasa los files de ese file con todas las rutas
-//app.use('/auth-jwt', jwtAuthRoutes );
+// Rutas de la API
 app.use(routes);
 
 const httpServer = app.listen(8080, () => {
-  console.log("Servidor escuchando en el puerto 8080");
-});
-
-// Configuramos socket
-export const io = new Server(httpServer);
-
-io.on("connection", (socket) => {
-  console.log("Nuevo usuario Conectado");
-});
+  console.log("Serv
