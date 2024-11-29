3f186101ff055b6c8cf7230d23bd15ab605795dc
SergioTeco <52510153+SergioTeco@users.noreply.github.com>
2024-11-29 10:50:19 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,38 +1,43 @@
 const express = require("express");
 const session = require("express-session");
-const passport = require("./config/passport"); // Estrategia local
-const { connectMongoDB } = require("./config/mondoDB");
-const routes = require("./routes"); // Importar las rutas desde index.js
+const passport = require("passport"); // Usamos el passport global para las estrategias
+const { connectMongoDB } = require("./config/mondoDB"); // Asegúrate de que el archivo de conexión esté bien configurado
+const routes = require("./routes"); // Importamos las rutas desde index.js
 const { Server } = require("socket.io");
 
-require("./config/passport-local");
-require("./config/passport-register");
-require("./config/passport-jwt");
+// Importamos las estrategias de Passport
+require("./config/passport-local");    // Estrategia de login con usuario/contraseña
+require("./config/passport-register"); // Estrategia de registro de usuarios
+require("./config/passport-jwt");      // Estrategia de autenticación JWT
+require("./config/passport-current");  // Estrategia de autenticación JWT actual (token en cookies)
 
+// Inicializamos el servidor Express
 const app = express();
 
-// Conexion a MongoDB
+// Conexión a MongoDB
 connectMongoDB();
 
+// Middleware para parsear cuerpos de las solicitudes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Configuración de sesión con estrategia local
 app.use(
   session({
-    secret: "secret",
-    resave: true, // Mantiene la sesión activa, si esto es false, la sesión se cierra
-    saveUninitialized: true, // Guarda la sesión
+    secret: "secret",            // Clave secreta para la sesión
+    resave: false,               // No volver a guardar la sesión si no ha cambiado
+    saveUninitialized: false,    // No guardar sesiones no inicializadas
   })
 );
 
 // Inicialización de Passport
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize());   // Inicializamos Passport
+app.use(passport.session());      // Establecemos la sesión de Passport
 
 // Rutas de la API
-app.use(routes);
+app.use(routes); // Las rutas que tienen acceso a las estrategias de autenticación
 
+// Conexión del servidor HTTP
 const httpServer = app.listen(8080, () => {
   console.log("Servidor escuchando en el puerto 8080");
 });
@@ -41,5 +46,5 @@ const httpServer = app.listen(8080, () => {
 const io = new Server(httpServer);
 
 io.on("connection", (socket) => {
-  console.log("Nuevo usuario Conectado");
+  console.log("Nuevo usuario conectado");
 });
